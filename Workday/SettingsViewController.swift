047a0f7c53e88c59a7c6edb1820b063bc0666c5a
Bjørn Rostad <hi@bjornrostad.no>
2014-10-22 16:36:49 -0700

    added fonts and more interactive settings screen

--- Workday/SettingsViewController.swift
+++ Workday/SettingsViewController.swift
@@ -13,6 +13,13 @@ class SettingsViewController: UIViewController {
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var settingsImage: UIImageView!
     
+    @IBOutlet weak var nameField: UITextField!
+    @IBOutlet weak var emailField: UITextField!
+    
+    @IBOutlet weak var eventsSwitch: UISwitch!
+    @IBOutlet weak var tasksSwitch: UISwitch!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,15 +38,5 @@ class SettingsViewController: UIViewController {
         
         dismissViewControllerAnimated(true, completion: nil)
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
