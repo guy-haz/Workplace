d794fed7d857360331980cbd737378f957061400
henryfreel <Henry@Henrys-MacBook-Pro-2.local>
2014-10-25 14:10:55 -0700

    launch screen

--- Workday/HomeViewController.swift
+++ Workday/HomeViewController.swift
@@ -23,11 +23,14 @@ class TodayViewController: UIViewController, UIScrollViewDelegate, UIGestureReco
     var task1Copy: UIImageView!
     var panGesture: UIPanGestureRecognizer!
     var originalPressDownLocation : CGPoint!
-
+    var originalImageCenter : CGPoint!
+    var taskFrame : CGRect!
     
     @IBOutlet var task1Gesture: UIPanGestureRecognizer!
     @IBOutlet var task2Gesture: UIPanGestureRecognizer!
     @IBOutlet var task3Gesture: UIPanGestureRecognizer!
+    
+    var taskLocation : CGPoint!
     
     var taskScrollViewIsScrolling = false
     
@@ -36,81 +39,6 @@ class TodayViewController: UIViewController, UIScrollViewDelegate, UIGestureReco
     var taskCenter: CGPoint!
     
     var imageView: UIImageView!
-    
-    @IBAction func onLongPress1(sender: UILongPressGestureRecognizer) {
-        
-        var location = sender.locationInView(view)
-        
-        
-        if sender.state == UIGestureRecognizerState.Began {
-            
-            println("longpress began at \(location)")
-            originalPressDownLocation = location
-            
-            //Create Copy of Task 1
-            task1Copy = UIImageView(frame: task1.frame)
-            task1Copy.image = task1.image
-            //transform it
-            UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: nil, animations:{ () -> Void in
-                self.task1Copy.transform = CGAffineTransformMakeScale(1.05, 1.05)
-            }, completion: nil)
-            
-            view.addSubview(task1Copy)
-            
-            
-            //Adding a shadow on the copy
-            task1Copy.layer.shadowColor = UIColor.darkGrayColor().CGColor
-            task1Copy.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
-            task1Copy.layer.shadowOpacity = 0.7
-            task1Copy.layer.shadowRadius = 2
-            task1Copy.layer.masksToBounds = true
-            task1Copy.clipsToBounds = false
-            
-            //we need to make sure the duplicate is placed in the right place at beginning, before it was only placed in the correct location on change
-            task1Copy.center.x = originalPressDownLocation.x
-            task1Copy.center.y = originalPressDownLocation.y
-            
-            
-        } else if sender.state == UIGestureRecognizerState.Changed {
-            
-            println("Changed long press at \(location)")
-            let translation = CGPoint(x:originalPressDownLocation.x - location.x, y:originalPressDownLocation.y - location.y)
-            
-            task1Copy.center.x = originalPressDownLocation.x - translation.x
-            task1Copy.center.y = originalPressDownLocation.y - translation.y
-            
-            //this will make the calendar scroll when the task is dragged to the bottom of the screen, and the velocity is taking into account the direction (downward in this case)
-            if task1Copy.center.y > 602 {
-                println("welcome to the bottom")
-                calendarScrollView.contentOffset.y  = calendarScrollView.contentOffset.y + 10
-            }
-            
-            else if task1Copy.center.y > 370 && task1Copy.center.y < 385{
-                println("welcome to the bottom")
-                calendarScrollView.contentOffset.y  = calendarScrollView.contentOffset.y - 10
-            }
-            
-            //if the user is moving the object up, the calendar should scroll up, the reason it is between 396 and 602, is in case the user goes back to the task scrollview, the bottom scrollview will stop scrolling
-            
-            
-            
-            
-         
-            
-            
-        } else if sender.state == UIGestureRecognizerState.Ended {
-            
-            //fade out the original copy when it is placed
-            UIView.animateWithDuration(0.2, animations: { () -> Void in
-                  self.task1Copy.alpha = 0
-            })
-          
-            
-            
-        }
-
-    
-    }
     
 //    func panGesture(sender: UIPanGestureRecognizer) {
 //        
@@ -140,10 +68,7 @@ class TodayViewController: UIViewController, UIScrollViewDelegate, UIGestureReco
 //        
 //    }
     
-    
 
-
-  
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -207,6 +132,92 @@ class TodayViewController: UIViewController, UIScrollViewDelegate, UIGestureReco
         return true
     }
     
+    @IBAction func onLongPress1(sender: UILongPressGestureRecognizer) {
+        
+        taskLocation = sender.locationInView(view)
+        
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            
+            task1Gesture.enabled = false
+            
+            task1.alpha = 0.5
+            
+            originalPressDownLocation = taskLocation
+            var originalImageCenter = task1.center
+            
+            //Create Copy of Task 1
+            taskFrame = view.convertRect(task1.frame, fromView: taskScrollView)
+            task1Copy = UIImageView(frame: taskFrame)
+            task1Copy.image = task1.image
+            
+            //transform it
+            UIView.animateWithDuration(0.3, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: nil, animations:{ () -> Void in
+                self.task1Copy.transform = CGAffineTransformMakeScale(1.05, 1.05)
+                }, completion: nil)
+            println("copied at \(taskLocation)")
+            
+            view.addSubview(task1Copy)
+            
+            //Adding a shadow on the copy
+            task1Copy.layer.shadowColor = UIColor.darkGrayColor().CGColor
+            task1Copy.layer.shadowOffset = CGSize(width: 1.0, height: 1.0)
+            task1Copy.layer.shadowOpacity = 0.7
+            task1Copy.layer.shadowRadius = 2
+            task1Copy.layer.masksToBounds = true
+            task1Copy.clipsToBounds = false
+            
+            //we need to make sure the duplicate is placed in the right place at beginning, before it was only placed in the correct location on change
+            //            task1Copy.center.x = originalPressDownLocation.x
+            //            task1Copy.center.y = originalPressDownLocation.y
+            //            task1Copy.frame.origin.x = location.x - originalImageCenter.x
+            //            task1Copy.frame.origin.y = location.y - originalImageCenter.y
+            //            task1Copy.center.x = originalImageCenter.x
+            //            task1Copy.center.y = originalImageCenter.y
+            
+            
+        } else if sender.state == UIGestureRecognizerState.Changed {
+            
+            println("Changed long press at \(taskLocation)")
+            let translation = CGPoint(x:originalPressDownLocation.x - taskLocation.x, y:originalPressDownLocation.y - taskLocation.y)
+            
+            //            task1Copy.center.x = originalPressDownLocation.x - translation.x
+            //            task1Copy.center.y = originalPressDownLocation.y - translation.y
+            task1Copy.frame.origin.x = taskFrame.origin.x - translation.x
+            task1Copy.frame.origin.y = taskFrame.origin.y - translation.y
+            
+            //this will make the calendar scroll when the task is dragged to the bottom of the screen, and the velocity is taking into account the direction (downward in this case)
+            if task1Copy.center.y > 602 && calendarScrollView.contentOffset.y < 1471{
+                println("welcome to the bottom")
+                calendarScrollView.contentOffset.y  = calendarScrollView.contentOffset.y + 10
+                println("\(calendarScrollView.contentOffset.y)")
+            }
+                
+            else if task1Copy.center.y > 370 && task1Copy.center.y < 400 && calendarScrollView.contentOffset.y > 0 {
+                println("welcome to the top")
+                calendarScrollView.contentOffset.y  = calendarScrollView.contentOffset.y - 10
+                println("\(calendarScrollView.contentOffset.y)")
+            }
+            
+            //if the user is moving the object up, the calendar should scroll up, the reason it is between 396 and 602, is in case the user goes back to the task scrollview, the bottom scrollview will stop scrolling
+            
+            
+        } else if sender.state == UIGestureRecognizerState.Ended {
+            
+            //fade out the original copy when it is placed
+            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                self.task1Copy.alpha = 0
+            })
+            
+            task1Gesture.enabled = true
+            task1.alpha = 1.0
+            
+        }
+        
+        
+    }
+
+    
     /*-------------TASK IS SCROLLING ON------------------------------*/
 
     
@@ -235,18 +246,22 @@ class TodayViewController: UIViewController, UIScrollViewDelegate, UIGestureReco
         var translation = sender.translationInView(view)
         var velocity = sender.velocityInView(view)
         
+        if translation.x > 0 || translation.x < 0 {
+            
+        }
+        
         if taskScrollViewIsScrolling == false {
             
             if sender.state == UIGestureRecognizerState.Began {
                 
                 taskCenter = tasktoSegue.center
-                taskScrollView.scrollEnabled = false
                 
             } else if sender.state == UIGestureRecognizerState.Changed {
                 
                 tasktoSegue.center.x = translation.x + taskCenter.x
                 var position = tasktoSegue.frame.origin.x
                 println("The position is \(position)")
+                taskScrollView.scrollEnabled = false
                 
                 
             } else if sender.state == UIGestureRecognizerState.Ended {
@@ -256,17 +271,21 @@ class TodayViewController: UIViewController, UIScrollViewDelegate, UIGestureReco
                 })
                 
                 taskScrollView.scrollEnabled = true
-
                 
             }
             
-            
         }
-        
-       
-        
+
         
     }
+    
+    
+    
+    
+    
+    
+    
+    
     
 //last one
 }
